f5004a320f2c36e2d9aa935308df10d38c8002ba
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-26 12:19:13 +0200

    Create Task7.1.1.cpp

---
+++ Task7.1.1.cpp
@@ -0,0 +1,25 @@
+/*3. 1.​ Написать функцию, которая возвращает истину, 
+если ее аргумент – простое целое число, и ложь, если 
+не простое. Простое число – это число, которое делиться 
+ТОЛЬКО на 1 и на себя (2, 5, 7, 11 и т.д.).*/
+
+#include <iostream>
+using namespace std;
+bool simple(int num);
+int main()
+{
+	setlocale()
+	int num;
+	cout << "Введите число: " ;
+	cin >> num;
+	if (simple(num)) cout << "true" << endl;
+	else cout << "false" << endl;	
+	system("pause");
+	return 0;
+}
+
+bool simple(int num) {
+	for (int i = 2; i < num; i++)
+		if (num%i == 0) return false;
+	return true;
+}
