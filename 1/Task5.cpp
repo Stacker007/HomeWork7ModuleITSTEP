2595833590076198652229aaf3a6f188395cbf64
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-29 19:35:57 +0300

    Create Task5.cpp

---
+++ 1/Task5.cpp
@@ -0,0 +1,38 @@
+/*5.​ Напишите функцию, которая принимает
+целое число и возвращает количество цифр 
+в числе и процент четных цифр.*/
+
+#include <iostream>
+using namespace std;
+int digits(int a, int *perPtr);
+int main()
+{	
+	setlocale(LC_ALL, "rus");
+	int num;
+	int count, percent = -1;
+	int *perPtr;
+	perPtr = &percent;
+	cout << "Введите число: ";
+	cin >> num;
+	count = digits(num, perPtr);
+	cout << "Количество цифр: " << count << endl;
+	cout << "процент четных цифр: " << percent << endl;
+	system("pause");
+	return 0;
+}
+int digits(int a, int *pPtr) {
+	int count = 0, countOfEven = 0;
+	if (a == 0) {
+		*pPtr = 100;
+		return 1;
+	}
+	while (a!=0)
+	{
+		count++;
+		if (a % 2 == 0) countOfEven++;
+		a /= 10;
+	}
+
+	*pPtr = countOfEven * 100 / count;
+	return count;
+}
