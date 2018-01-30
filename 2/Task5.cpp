b7a2068054cdccca34822f1c3afa6534c219f2e9
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-30 20:48:21 +0300

    Update Task5.cpp

--- 2/Task5.cpp
+++ 2/Task5.cpp
@@ -10,20 +10,43 @@ double R = calc(‘s’, 5, 3, 4, 5, 3, 7);
 
 cout << R;
 
-На экране мы увидим 4.4.
+На экране мы увидим 4.5.
 */
 
 #include <iostream>
-double calc(char a, int,...);
+double calc(char ch, int a,...);
 using namespace std;
 
 int main()
 {	
 	setlocale(LC_ALL, "rus");
-	
+	double R = calc('*', 5, 3, 4, 5, 3, 7, 0);
+
+	cout << R << endl;
 	system("pause");
 	return 0;
 }
-double calc(char a, int, ...) {
-
+double calc(char ch, int a, ...) {
+	int sum = 0, prod = 1;
+	int count = 0;
+	int *ptr;
+	ptr = &a;
+	while (*ptr)
+	{
+		sum += *ptr++;
+		count++;
+	}
+	ptr = &a;
+	while (*ptr)
+	{
+		prod *= *ptr++;
+	}
+	switch (ch)
+	{
+	case '+': return sum; break;
+	case '*': return prod; break;
+	case 's': return double(sum)/count ; break;
+	}
+	cout << "Ошибка!" << endl;
+	return -1;
 }
