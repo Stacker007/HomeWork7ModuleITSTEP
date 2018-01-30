3d46c314032dbf1797a05ee5a4ab9397574ba0e6
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-30 18:44:44 +0300

    Update Task2.cpp

--- 2/Task2.cpp
+++ 2/Task2.cpp
@@ -3,42 +3,49 @@
 местами первый отрицательный элемент и последний положительный. 
 Преобразованный массив также вывести на консоль. При работе с массивом 
 использовать только указатели! Оформить в виде отдельных функций: 
-1) инициализацию массива, 
-2) вывод на консоль, 
-3) поиск первого отрицательного, 
++1) инициализацию массива, 
++2) вывод на консоль, 
++3) поиск первого отрицательного, 
 4) последнего положительного и 
 5) обмен местами двух элементов массива.*/
 
 #include <iostream>
 #include <ctime>
-#define SIZE 10
+#define SIZE 50
 using namespace std;
 void randArr(int *ptr, int sizeArr);  //1) инициализацию массива, 
 void printArr(int *ptr, int sizeArr); //2) вывод на консоль, 
 int firstNegative(int *ptr, int sizeArr); //3) поиск первого отрицательного, 
+int lastPositive(int *ptr, int sizeArr);
 int main()
 {	
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
 	int oneRowArr[SIZE] = {};
 	int *fPtr, *lPtr;
-	int iF;
-		
+	int indexOfFN;		
 	cout << "Исходный линейный массив: " << endl;
 	randArr(oneRowArr, SIZE);
 	printArr(oneRowArr, SIZE);
-
-	 = firstNegative(int *ptr, int sizeArr);
-
+	cout << endl;
+	indexOfFN= firstNegative(oneRowArr, SIZE);
+	fPtr = oneRowArr + indexOfFN;
+	indexOfFN = lastPositive(oneRowArr, SIZE);
+	lPtr = oneRowArr + indexOfFN;
+	int tmp = *fPtr;
+	*fPtr = *lPtr;
+	*lPtr = tmp;
+	cout << "Итоговый массив: " << endl;
+	printArr(oneRowArr, SIZE);
+	cout << endl;
 	system("pause");
 	return 0;
 }
 
 
 void randArr(int *ptr, int sizeArr) {
-
-	for (int i = 0; i<sizeArr; i++, ptr++)
-		*ptr = rand() % 21 - 10;
+	for (int i = 0; i < sizeArr; i++, ptr++)
+		*ptr = rand() % 19 - 9;
 }
 
 void printArr(int *ptr, int sizeArr) {
@@ -47,6 +54,12 @@ void printArr(int *ptr, int sizeArr) {
 }
 
 int firstNegative(int *ptr, int sizeArr) {
+	for (int i=0; i < sizeArr; i++, ptr++)
+		if (*ptr<0) return i;
+}
 
-	return i;
+int lastPositive(int *ptr, int sizeArr) {
+	ptr = ptr + sizeArr - 1;
+	for (int i = sizeArr - 1; i >= 0; i--, ptr--)
+		if (*ptr > 0) return i;
 }
