496dc08c202165a5657f72c79dae184f98ead599
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-04 10:09:05 +0300

    Update Task7.cpp

--- 3/Task7.cpp
+++ 3/Task7.cpp
@@ -1,40 +1,54 @@
-/*7.​ Написать рекурсивный 
-алгоритм сортировки методом выбора.*/
+/*
+7.​ Написать рекурсивный алгоритм сортировки методом выбора.
+*/
 
 #include <iostream>
 #include <ctime>
-#define SIZE 20
+#define SIZE 10
 using namespace std;
 void randArr(int *a, int sizeArr);
 void printArr(int *a, int sizeArr);
-void sort(int *a, int size);
-
-int main() {
+void sortByPaste(int *arrPtr, int size);
+int main()
+{
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
-	int arrA[SIZE];
-	//Генерация массива А[n]:
+	int arrA[SIZE];    
+	//Генерация  массива  А[n]:
+	//(Инициализируем случайными числами)
 	{
 		randArr(arrA, SIZE);
-		cout << "Несортированный массив А[n]:" << endl;
+		cout << "Исходный массив А[n]:" << endl;
 		printArr(arrA, SIZE);
 	}
-
-
+    int *endPtr = arrA+SIZE;
+	sortByPaste(arrA,endPtr);
+	printArr(arrA, SIZE);
 	system("pause");
+	return 0;
 }
-void sort(int *a, int size) {
-	void searchMax(int *now,int *end);
 
-
+void sortByPaste(int *arrPtr, int *endPtr){
+    int *maxPtr=arrPtr;
+    int *ptr=arrPtr;
+    while (ptr<endPtr){
+        if (*ptr>*maxPtr) *maxPtr=*ptr;
+        ptr++;
+    }
+    int tmp =*arrPtr;
+    *arrPtr=*maxPtr;
+    *maxPtr=tmp;
+    arrPtr++;
+    sortByPste(arrPtr,endPtr);
 }
 
 void randArr(int *a, int sizeArr) {
 	for (int i = 0; i<sizeArr; i++, a++)
-		*a = rand() % 201-100;
+		*a = rand() % 21 - 10;
 }
 void printArr(int *a, int sizeArr) {
 	for (int i = 0; i < sizeArr; i++, a++)
-		printf("%4d ", *a);
+		cout << *a << " ";
 	cout << endl;
 }
+
